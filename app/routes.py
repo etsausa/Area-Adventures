848d153db4f33ff02fb342a58ad2c70d551a5e9f
Lauren Suna <laurensuna@gmail.com>
2019-11-19 10:22:15 -0500

    built login route

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,16 @@
 from app import app
 from flask import render_template, flash, redirect, url_for, request
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html', title='Home')
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember me={}'.format(form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
