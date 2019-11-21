82f8a3ee9989f1ed2f425b8e8e0702fe6a3584dd
Lauren Suna <laurensuna@gmail.com>
2019-11-21 10:26:37 -0500

    working registration and login as well as some changes to the DB

--- app/models.py
+++ app/models.py
@@ -4,23 +4,38 @@ from app import login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
 
-    submittedPosts = db.relationship('Post', backref='author', lazy='dynamic')
-    savedPosts = db.relationship('Post', backref='saved', lazy='dynamic')
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+    def set_password(self, passsword):
+        self.password_hash = generate_password_hash(passsword)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(64), index=True)
     description = db.Column(db.String(512), index=True)
     timeStamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    is_submitted = db.Column(db.Boolean)
+    user_id =  db.Column(db.Integer, db.ForeignKey(User.id))
+
 
     def __repr__(self):
         return '<Post {}>'.format(self.title)
@@ -30,7 +45,7 @@ class Location(db.Model):
     Long = db.Column(db.Float, index=True)
     Lat = db.Column(db.Float, index=True)
 
-    post = db.relationship('Post', backref='location', lazy='dynamic')
+    #post = db.relationship('Post', backref='location', lazy='dynamic')
 
     def __repr__(self):
         return '<location {}>'.format(self.post)
