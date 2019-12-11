0c830bf45c66ca31a20c9dba619204013e7e3623
Lauren Suna <laurensuna@gmail.com>
2019-12-10 19:56:44 -0500

    started adding bootstrap

--- app/routes.py
+++ app/routes.py
@@ -36,7 +36,7 @@ def submit():
     form = PostForm()
     if form.validate_on_submit():
 
-        p = Post(title=form.loc_name.data, description=form.description.data, long=form.longitude.data, lat=form.latitude.data)
+        p = Post(title=form.loc_name.data, description=form.description.data, Long=form.longitude.data, Lat=form.latitude.data)
 
         db.session.add(p)
         db.session.commit()
